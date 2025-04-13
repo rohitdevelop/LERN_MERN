e6f7851c5561ddcabb720b8dbc73d6d437182f4f
Rohit singh <rohitdev124421@gmail.com>
2025-04-13 14:47:58 +0530

    done

--- src/Component/Home/Hometext.jsx
+++ src/Component/Home/Hometext.jsx
@@ -6,7 +6,7 @@ Link
   return (
     <div className="text-center">
       <h1 className="text-4xl md:text-6xl font-bold">LERN_MERN</h1>
-      <h2 className="text-xl md:text-2xl mt-2">MAN SE</h2>
+      <h2 className="text-xl md:text-2xl mt-2">MAN SE calo shuru kare </h2>
       <Link to={'/chapters'}>
       <button className="mt-4 px-6 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition">
         Let's Start
