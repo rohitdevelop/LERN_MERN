2b75d18dc05f7029cea3432adb88eecf3ad26e05
Rohit singh <rohitdev124421@gmail.com>
2025-04-28 13:42:54 +0530

    done mongo

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server:{
   proxy:{
-    '/api': 'http://localhost:3000',
+    '/api': 'https://lern-backend.onrender.com',
   }
   },
   plugins: [react(),tailwindcss()],
