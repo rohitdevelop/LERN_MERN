ea39cf341d68010713d2e497906c9e6029c07698
Rohit singh <rohitdev124421@gmail.com>
2025-06-28 23:32:02 +0530

    Ai chatbot

--- src/Component/AI/Chatbot.jsx
+++ src/Component/AI/Chatbot.jsx
@@ -1,20 +1,19 @@
-// src/Chatbot.jsx
 import React from "react";
 
 const Chatbot = () => {
   return (
-    <div className="w-full min-h-screen bg-gradient-to-br from-purple-950 via-black to-gray-900 pt-24 p-4 flex flex-col items-center">
-     <div className="w-full text-center flex items-center justify-center max-w-5xl h-[700px] shadow-lg rounded-lg overflow-hidden border border-gray-500">
+    <div className="w-full min-h-screen bg-gradient-to-br from-purple-950 via-black to-gray-900 pt-24 px-4 flex flex-col items-center">
+            <div className="w-full max-w-5xl h-[80vh] sm:h-[700px] bg-black/30 shadow-xl rounded-2xl overflow-hidden border border-gray-600 backdrop-blur-md">
         <iframe
           src="https://www.chatbase.co/chatbot-iframe/fZBzCKeWQntstAtNVb-AF"
-          width="70%"
-          height="98%"
+          width="100%"
+          height="100%"
           style={{ border: "none" }}
           title="Chatbot"
           allow="clipboard-write"
         ></iframe>
       </div>
-    </div>
+     </div>
   );
 };
 
